6c2aba886245ea7325fe0c6d96670293515c24ae
faduzin <eric.fadul@gmail.com>
2025-02-06 23:39:03 -0300

    Far from the end

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from src.data_preprocessing import clean_data, label_encode, data_normalize, preprocess_data, split_data
 from src.evaluating import compare_models, evaluate_kl_divergence, evaluate_reconstruction, generate_samples
 from src.utils import load_data, load_model, plot_loss, save_data, save_model, visualize_latent_space, data_info
-from src.vae import build_vae, train_vae
+from src.vae import build_encoder, build_decoder, model_checkpoint_callback, predict, plot_latent_space, plot_reduced_pca, VAE
 
 __all__ = [
     'clean_data',
@@ -20,6 +20,11 @@ __all__ = [
     'save_model',
     'visualize_latent_space',
     'data_info',
-    'build_vae',
-    'train_vae'
+    'build_encoder', 
+    'build_decoder', 
+    'model_checkpoint_callback', 
+    'predict', 
+    'plot_latent_space', 
+    'plot_reduced_pca',
+    'VAE'
 ]
--- src/vae.py
+++ src/vae.py
@@ -1,111 +1,184 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import tensorflow as tf
+import tensorflow.keras.backend as K
+from scipy.stats import norm
+from tensorflow.keras import (
+    layers, 
+    models,
+    metrics, 
+    optimizers, 
+    losses, 
+    callbacks
+)
+from sklearn.decomposition import PCA
 
-# Define sampling layer
-class Sampling(tf.keras.layers.Layer):
+class Sampling(layers.Layer):
     def call(self, inputs):
-        mean, log_var = inputs
-        epsilon = tf.random.normal(shape=tf.shape(mean))
-        return mean + tf.exp(0.5 * log_var) * epsilon
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = K.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+    
+def build_encoder(latent_dim, input_shape, topology=[256, 128, 64]):
+    encoder_inputs = layers.Input(
+        shape=(input_shape,), name="encoder_input_layer"
+    )
 
-def build_encoder(input_shape, latent_dim):
-    # Define encoder
-    try:
-        encoder = tf.keras.Sequential([
-            tf.keras.layers.InputLayer(shape=input_shape),
-            tf.keras.layers.Dense(64, activation='relu'),
-            tf.keras.layers.Dense(32, activation='relu'),
-            tf.keras.layers.Dense(latent_dim)
-        ])
-        print('Encoder built successfully.')
-    except:
-        print('Error building encoder')
-        return None
+    x = layers.Dense(topology[0], activation='relu')(encoder_inputs)
+    x = layers.Dense(topology[1], activation='relu')(x)
+    x = layers.Dense(topology[2], activation='relu')(x)
+
+    z_mean = layers.Dense(latent_dim, name="z_mean_layer")(x)
+    z_log_var = layers.Dense(latent_dim, name="z_log_var_layer")(x)
+
+    z = Sampling()([z_mean, z_log_var])
+
+    encoder = models.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+    encoder.summary()
     
     return encoder
 
 
-def build_decoder(latent_dim, output_shape):
-    # Define decoder
-    try:
-        decoder = tf.keras.Sequential([
-            tf.keras.layers.InputLayer(shape=(latent_dim,)),
-            tf.keras.layers.Dense(64, activation='relu'),
-            tf.keras.layers.Dense(128, activation='relu'),
-            tf.keras.layers.Dense(output_shape[0], activation='softmax'),
-        ])
-        print('Decoder built successfully.')
-    except:
-        print('Error building decoder.')
-        return None
+def build_decoder(latent_dim, input_shape, topology=[256, 128, 64]):
+    decoder_inputs = layers.Input(shape=(latent_dim,), name="decoder_input_layer")
+
+    x = layers.Dense(topology[2], activation='relu')(decoder_inputs)
+    x = layers.Dense(topology[1], activation='relu')(x)
+    x = layers.Dense(topology[0], activation='relu')(x)
+
+    decoder_outputs = layers.Dense(input_shape, activation='sigmoid')(x)
+
+    decoder = models.Model(decoder_inputs, decoder_outputs, name="decoder")
+    decoder.summary()
     
     return decoder
 
+class VAE(models.Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.total_loss_tracker = metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = metrics.Mean(name="reconstruction_loss")
+        self.kl_loss_tracker = metrics.Mean(name="kl_loss")
 
-def build_vae(input_shape, latent_dim, beta=1.0):
-    # Define VAE
-    encoder = build_encoder(input_shape, latent_dim)
-    decoder = build_decoder(latent_dim, input_shape)
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker
+        ]
+    
+    def call(self, inputs):
+        z_mean, z_log_var, z = self.encoder(inputs)
+        reconstructed = self.decoder(z)
+        return z_mean, z_log_var, reconstructed
+    
+    def train_step(self, data):
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, reconstruction = self(data)
+            beta = 1
+            reconstruction_loss = tf.reduce_mean(
+                beta * losses.binary_crossentropy(data, reconstruction, axis=1)
+            )
+            kl_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)), axis=1
+                )
+            )
 
-    if encoder is None or decoder is None:
-        return None
+            total_loss = reconstruction_loss + kl_loss
+        
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+
+        return { m.name: m.result() for m in self.metrics }
     
-    # Define VAE model
-    try:
-        inputs = tf.keras.layers.Input(shape=input_shape)
-        encoder_output = encoder(inputs)
-        mean_log_var = tf.keras.layers.Lambda(lambda x: tf.split(x, num_or_size_splits=2, axis=1))(encoder_output)
-        mean = mean_log_var[0]
-        log_var = mean_log_var[1]
-        z = Sampling()([mean, log_var])
-        outputs = decoder(z)
-        vae = tf.keras.Model(inputs, outputs)
-        print('VAE built successfully.')
-    except Exception as e:
-        print(f'Error building VAE: {e}')
-        return None
-    
-    # Define loss
-    try:
-        # Reconstruction loss (use vae.input instead of `inputs`)
+    def test_step(self, data):
+        if isinstance(data, tuple):
+            data = data[0]
+
+        z_mean, z_log_var, reconstruction = self(data)
+        beta = 1
         reconstruction_loss = tf.reduce_mean(
-            tf.keras.losses.binary_crossentropy(vae.input, vae.output)
+            beta * losses.binary_crossentropy(data, reconstruction, axis=1)
+        )
+        kl_loss = tf.reduce_mean(
+            tf.reduce_sum(
+                -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)), axis=1
+            )
         )
 
-        # KL Divergence loss
-        kl_loss = tf.reduce_mean(-0.5 * tf.reduce_sum(1 + log_var - tf.square(mean) - tf.exp(log_var), axis=1))
+        total_loss = reconstruction_loss + kl_loss
 
-        # Final loss
-        vae_loss = reconstruction_loss + beta * kl_loss
-
-        # Add loss before compiling
-        vae.add_loss(vae_loss)
-
-        # Compile VAE
-        vae.compile(optimizer=tf.keras.optimizers.Adam())
-        print('Loss defined successfully.')
-    except:
-        print('Error defining loss.')
-        return None
-    
-    return vae
+        return {
+            "total_loss": total_loss,
+            "reconstruction_loss": reconstruction_loss,
+            "kl_loss": kl_loss
+        }
 
 
-def train_vae(vae, 
-              data, 
-              epochs=100,
-              optimizer='adam', 
-              batch_size=32):
-    
-    match optimizer:
-        case 'adam':
-            pass
-        case 'rmsprop':
-            pass
-        case _:
-            print('Invalid optimizer')
-            return None
-    
-    vae.compile(optimizer=optimizer)
-    history = vae.fit(data, epochs=epochs, batch_size=batch_size)
-    
-    return history
+def model_checkpoint_callback(filepath):
+    model_checkpoint = callbacks.ModelCheckpoint(
+        filepath=filepath,
+        save_best_only=True,
+        save_weights_only=False,
+        save_freq="epoch",
+        monitor="total_loss",
+        mode="min",
+        verbose=0
+    )
+    print("Model checkpoint callback created.")
+    return model_checkpoint
+
+
+def predict(vae, data, label, n_to_predict=1000):
+    n_to_predict = 5000
+    example = data[:n_to_predict]
+    example_labels = label[:n_to_predict]
+    z_mean, z_log_var, reconstructions = vae.predict(example)
+    return z_mean, z_log_var, reconstructions, example_labels
+
+
+def plot_latent_space(encoder, example, example_labels):
+    z_mean, z_var, z = encoder.predict(example)
+    p = norm.cdf(z)
+
+    figsize = 8
+    fig = plt.figure(figsize=(figsize * 2, figsize))
+    ax = fig.add_subplot(1, 2, 1)
+    plot_1 = ax.scatter(
+        z[:, 0], z[:, 1], cmap="rainbow", c=example_labels, alpha=0.8, s=15
+    )
+    plt.colorbar(plot_1)
+    ax = fig.add_subplot(1, 2, 2)
+    plot_2 = ax.scatter(
+        p[:, 0], p[:, 1], cmap="rainbow", c=example_labels, alpha=0.8, s=15
+    )
+    plt.show()
+
+
+def plot_reduced_pca(data, labels):
+    # Reduce to 2D using PCA
+    pca = PCA(n_components=2)
+    reduced_data = pca.fit_transform(data)
+
+    # Scatter plot of the PCA projection
+    plt.figure(figsize=(8, 6))
+    plt.scatter(reduced_data[:, 0], reduced_data[:, 1], c=labels, cmap="coolwarm", alpha=0.7)
+    plt.colorbar(label="Class Labels")
+    plt.xlabel("Principal Component 1")
+    plt.ylabel("Principal Component 2")
+    plt.title("PCA Visualization of Encoded Data")
+    plt.show()
+
+    # Print explained variance ratio
+    print(f"Explained variance ratio: {pca.explained_variance_ratio_}")
