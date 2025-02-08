e38fee282128355705d334096a194cbba5dcc470
faduzin <eric.fadul@gmail.com>
2025-02-08 00:55:42 -0300

    functions added to package

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from src.data_preprocessing import clean_data, label_encode, data_normalize, preprocess_data, split_data
 from src.evaluating import compare_models, evaluate_kl_divergence, evaluate_reconstruction, generate_samples
 from src.utils import load_data, load_model, plot_loss, save_data, save_model, visualize_latent_space, data_info
-from src.vae import build_encoder, build_decoder, model_checkpoint_callback, predict, plot_latent_space, plot_reduced_pca, VAE
+from src.vae import LossTracker, plot_loss, optimizer_adam, build_encoder, build_decoder, model_checkpoint_callback, predict, plot_latent_space, plot_reduced_pca, VAE
 
 __all__ = [
     'clean_data',
@@ -26,5 +26,8 @@ __all__ = [
     'predict', 
     'plot_latent_space', 
     'plot_reduced_pca',
-    'VAE'
+    'VAE',
+    'optimizer_adam',
+    'LossTracker',
+    'plot_loss'
 ]
