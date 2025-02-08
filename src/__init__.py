fbbf353d0d6380226056ebc3a4cdf0e6ee40bcd5
faduzin <eric.fadul@gmail.com>
2025-02-08 12:20:49 -0300

    removing unused functions

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from src.data_preprocessing import clean_data, label_encode, data_normalize, preprocess_data, split_data
 from src.evaluating import compare_models, evaluate_kl_divergence, evaluate_reconstruction, generate_samples
-from src.utils import load_data, load_model, plot_loss, save_data, save_model, visualize_latent_space, data_info
+from src.utils import plot_correlations, load_data, plot_loss, save_data,  data_info
 from src.vae import plot_pairplot, LossTracker, plot_loss, optimizer_adam, build_encoder, build_decoder, model_checkpoint_callback, predict, plot_latent_space, plot_reduced_pca, VAE
 
 __all__ = [
@@ -14,11 +14,7 @@ __all__ = [
     'evaluate_reconstruction',
     'generate_samples',
     'load_data',
-    'load_model',
-    'plot_loss',
     'save_data',
-    'save_model',
-    'visualize_latent_space',
     'data_info',
     'build_encoder', 
     'build_decoder', 
@@ -30,5 +26,6 @@ __all__ = [
     'optimizer_adam',
     'LossTracker',
     'plot_loss',
-    'plot_pairplot'
+    'plot_pairplot',
+    'plot_correlations'
 ]
